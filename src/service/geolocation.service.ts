51088aad3b0b636b15e8db895e3bf3e826cbf164
Shreyas <ShreyasRao99>
2023-11-06 14:54:51 +0530

    meta tags added, ui changes, bug fixes

--- src/service/geolocation.service.ts
+++ src/service/geolocation.service.ts
@@ -1,37 +1,41 @@
 import { Injectable } from '@angular/core';
+import { LoaderstatusService } from 'src/app/mainloader/loaderstatus.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GeolocationService {
 
-  constructor() { }
+    constructor(private loaderstatusService: LoaderstatusService) { }
 
-  getCurrentCoordinate(hideSaved: boolean,showloader: boolean){
-    return new Promise(async (resolve, reject) => {
-        try{
-            console.log('getCurrentCoordinate called hideSaved',hideSaved); 
-                const coordinates = await this.getCenter(showloader);           
+    getCurrentCoordinate(hideSaved: boolean, showloader: boolean) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                this.loaderstatusService.show();
+                console.log('getCurrentCoordinate called hideSaved', hideSaved);
+                const coordinates = await this.getCenter(showloader);
+                this.loaderstatusService.hide();
                 resolve(coordinates);
-        }catch (error){
-            console.log('getCurrentCoordinate error ',error); 
-            resolve({});
-        }
-    });
-}
-
-private async getCenter(showloader: boolean){
-  return new Promise(async (resolve, reject) => {
-    try{
-        navigator.geolocation.getCurrentPosition((coordinates)=>{
-            console.log('$$$$$$$',coordinates)
-            resolve({lat: coordinates.coords.latitude, lng: coordinates.coords.longitude})
-        });            
-    }catch (e){
-        console.log('error while fetching current position ', e);
-        reject(e);
+            } catch (error) {
+                console.log('getCurrentCoordinate error ', error);
+                this.loaderstatusService.hide();
+                resolve({});
+            }
+        });
     }
-});
-}
+
+    private async getCenter(showloader: boolean) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                navigator.geolocation.getCurrentPosition((coordinates) => {
+                    console.log('$$$$$$$', coordinates)
+                    resolve({ lat: coordinates.coords.latitude, lng: coordinates.coords.longitude })
+                });
+            } catch (e) {
+                console.log('error while fetching current position ', e);
+                reject(e);
+            }
+        });
+    }
 
 }
